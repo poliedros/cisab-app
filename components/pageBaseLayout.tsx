7a3b7114b495f0633d8b0859fcf820e0c5520d34
MrAndersonMR <sr.andersonmendesribeiro@gmail.com>
2022-10-21 19:09:21 -0300

    fix error in pageBaseLayout

--- components/pageBaseLayout.tsx
+++ components/pageBaseLayout.tsx
@@ -20,7 +20,7 @@ export default function PageBaseLayout(
             {show && type === "main" ? (
                 <div className="h-screen w-100 overflow-auto p-6 slide-in-bottom invisibleScroll">
                     <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-screen sm:rounded-3xl sm:px-10">
-                        <CountyRegistration language={"pt"} />
+                        <CountyRegistration language={"pt"} county={undefined}/>
                         <CountyList />
                         <Home language="pt" />
                     </div>
