32cd1a0c26570165011df1d95644452cddcef3a7
Carlos <carloshlzansavio@gmail.com>
2022-10-21 14:53:51 -0300

    add translations interface

--- atoms/capSubtitle.tsx
+++ atoms/capSubtitle.tsx
@@ -1,15 +1,17 @@
-import translations from "../translations.json";
+import translations from "../lib/translations";
 
 export default function CapSubtitle({
-    label = "emptyText",
-    language = "pt",
+  label = "emptyText",
+  language = "pt",
 }: {
-    label?: string;
-    language?: "pt";
+  label?: string;
+  language?: "pt";
 }) {
-    return <>
-        <h5 className="pl-4 text-[#7dc523] text-right uppercase tracking-widest p-2 mt-6 mb-4 border-b border-[#7dc523]">
-            {(translations[label])[language]}
-        </h5>
-    </>;
+  return (
+    <>
+      <h5 className="pl-4 text-[#7dc523] text-right uppercase tracking-widest p-2 mt-6 mb-4 border-b border-[#7dc523]">
+        {translations(label, language)}
+      </h5>
+    </>
+  );
 }
