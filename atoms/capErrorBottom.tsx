350d8aa8f129c61858c0cc9d918a657899ab8446
MrAndersonMR <sr.andersonmendesribeiro@gmail.com>
2022-10-25 14:56:56 -0300

    margin fix in capError

--- atoms/capErrorBottom.tsx
+++ atoms/capErrorBottom.tsx
@@ -13,7 +13,7 @@ export default function CapErrorBottom({
     <>
         <div className="absolute bottom-3 flex items-center justify-between swing-in-right-bck">
             <div className="relative bg-white p-3 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-screen sm:rounded-3xl sm:px-10 w-max h-max">
-                <h6 className="font-medium leading-tight mt-0 mb-2 text-red-600">
+                <h6 className="font-medium leading-tight m-0 text-red-600">
                     {literal ? literal : translations(label, language)}
                 </h6>
             </div>
