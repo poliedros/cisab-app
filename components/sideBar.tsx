e4d4b0bd370b9806657e92190d50aff8ddbcee70
MrAndersonMR <sr.andersonmendesribeiro@gmail.com>
2022-11-21 19:51:20 -0300

    new testPage and labComponent for Test

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -67,7 +67,31 @@ export default function SideBar(/* { language }: { language: "en" | "es" | "pt" 
                         iconType="md"
                         icon="MdLinearScale"
                         size="24px"
-                        //route="/counties"
+                        route="/counties"
+                        hoverColor="#7dc523"
+                    />
+                </div>
+            </div>
+        </Popover>
+    );
+
+    const project = (
+        <Popover>
+            <div className="overflow-auto -m-6 p-4 invisibleScroll">
+                <div className="flex relative bg-white px-4 pt-4 pb-4 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-screen sm:rounded-3xl sm:px-5">
+                    <CapIconButton
+                        iconType="bs"
+                        icon="BsDiagram2Fill"
+                        size="24px"
+                        route="/products/create"
+                        hoverColor="#7dc523"
+                    />
+                    &nbsp; &nbsp;
+                    <CapIconButton
+                        iconType="ri"
+                        icon="RiTestTubeFill"
+                        size="24px"
+                        route="/test"
                         hoverColor="#7dc523"
                     />
                 </div>
@@ -103,6 +127,20 @@ export default function SideBar(/* { language }: { language: "en" | "es" | "pt" 
                         <CapIconButton
                             iconType="gi"
                             icon="GiCardboardBoxClosed"
+                        />
+                    </div>
+                </OverlayTrigger>
+                &nbsp;
+                <OverlayTrigger
+                    trigger="click"
+                    placement="right"
+                    overlay={project}
+                    rootClose
+                >
+                    <div>
+                        <CapIconButton
+                            iconType="bs"
+                            icon="BsDiagram3Fill"
                         />
                     </div>
                 </OverlayTrigger>
