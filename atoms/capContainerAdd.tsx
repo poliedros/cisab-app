70aa1196a09ff5af3da05aa40853cbc2b693f860
Carlos <carloshlzansavio@gmail.com>
2022-11-29 18:17:45 -0300

    comment broken code

--- atoms/capContainerAdd.tsx
+++ atoms/capContainerAdd.tsx
@@ -28,42 +28,40 @@ export default function CapContainerAdd({
     setListComponents([...listComponents, components]);
   };
 
+  // TODO:
   const handleArrayMinus = (i: number) => {
-    document.getElementById(String(i))?.remove();
-    const list = document.getElementById("list");
-    /* let finalArray: string[] = [];
-        list?.childNodes.forEach(function (node, index) {
-            finalArray.push(
-                node.childNodes[2].firstChild?.firstChild?.firstChild
-                    ?.textContent
-                    ? node.childNodes[2].firstChild?.firstChild?.firstChild
-                          ?.textContent
-                    : ""
-            );
-        }); */
-    let finalArray: { name: string; value: string; unit: string }[] = [];
-    list?.childNodes.forEach(function (node, index) {
-      // TODO:
-      // finalArray.push({
-      //   name: node.childNodes[0].firstChild?.lastChild?.textContent
-      //     ? node.childNodes[0].firstChild?.lastChild?.value
-      //     : "",
-      //   value: node.childNodes[1].firstChild?.lastChild?.textContent
-      //     ? node.childNodes[1].firstChild?.lastChild?.value
-      //     : "",
-      //   unit: node.childNodes[2].firstChild?.firstChild?.firstChild?.textContent
-      //     ? node.childNodes[2].firstChild?.firstChild?.firstChild?.textContent
-      //     : "",
-      // });
-    });
-    list?.childNodes.forEach(function (node, index) {
-      node.childNodes[1].firstChild?.lastChild?.textContent
-        ? console.log(node.childNodes[1].firstChild?.lastChild?.value)
-        : "";
-    });
-    console.log(finalArray);
-    setResultArray(finalArray);
-    console.log(resultArray);
+    // document.getElementById(String(i))?.remove();
+    // const list = document.getElementById("list");
+    // let finalArray: string[] = [];
+    // list?.childNodes.forEach(function (node, index) {
+    //   finalArray.push(
+    //     node.childNodes[2].firstChild?.firstChild?.firstChild?.textContent
+    //       ? node.childNodes[2].firstChild?.firstChild?.firstChild?.textContent
+    //       : ""
+    //   );
+    // });
+    // let finalArray: { name: string; value: string; unit: string }[] = [];
+    // list?.childNodes.forEach(function (node, index) {
+    // finalArray.push({
+    //   name: node.childNodes[0].firstChild?.lastChild?.textContent
+    //     ? node.childNodes[0].firstChild?.lastChild?.value
+    //     : "",
+    //   value: node.childNodes[1].firstChild?.lastChild?.textContent
+    //     ? node.childNodes[1].firstChild?.lastChild?.value
+    //     : "",
+    //   unit: node.childNodes[2].firstChild?.firstChild?.firstChild?.textContent
+    //     ? node.childNodes[2].firstChild?.firstChild?.firstChild?.textContent
+    //     : "",
+    // });
+    // });
+    // list?.childNodes.forEach(function (node, index) {
+    //   node.childNodes[1].firstChild?.lastChild?.textContent
+    //     ? console.log(node.childNodes[1].firstChild?.lastChild?.value)
+    //     : "";
+    // });
+    // console.log(finalArray);
+    // setResultArray(finalArray);
+    // console.log(resultArray);
   };
 
   return (
