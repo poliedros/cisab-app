9abac00ef44fe9f78427bd2de211badcb3c67c54
Daniela Gomes <daniela.souza.gomes@outlook.com>
2022-11-30 16:23:53 -0300

    Pagina de email para recuperar senha e requisicoes

---
+++ pages/api/forgetPassword.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<boolean>) {
+  if (req.method === "POST") {
+    const response = await fetch(process.env.API_URL + "/forget-password/", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: req.body,
+    });
+    const data = (await response.json()) as boolean;
+    res.status(response.status).json(data);
+    return;
+  }
+}
+
+export default handler;
