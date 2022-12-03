9adaa8d9f943c8fcf51710b536d8d4f08ebcc928
MrAndersonMR <sr.andersonmendesribeiro@gmail.com>
2022-12-03 00:11:51 -0300

    rollback to Daniela commit, just layout change

--- components/registration/account.tsx
+++ components/registration/account.tsx
@@ -1,174 +1,64 @@
-import { Formik } from "formik";
-import * as yup from "yup";
-import CapFormik from "atoms/capFormik";
-import CapBtn from "atoms/capBtn";
+import CapForm from "atoms/capForm";
+import CapSubtitle from "atoms/capSubtitle";
+import { CountyManagerDTO } from "pages/api/counties/[id]/manager";
 import { useState } from "react";
-import { Row, Col, Form } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 type AccountProps = {
-    language?: "pt";
-    type: "county" | "autarky";
-    setCurrentStep?: any;
-    setInstitutionAccount?: any;
-    registerAccount?: any;
+  language: "pt";
+  kind: "county" | "autarky";
+  handleAccount: (
+    account: CountyManagerDTO,
+    kind: "county" | "autarky"
+  ) => void;
 };
 
 export default function Account({
-    language = "pt",
-    type,
-    setCurrentStep,
-    setInstitutionAccount,
-    registerAccount,
+  language = "pt",
+  kind,
+  handleAccount,
 }: AccountProps) {
-    const [step, setStep] = useState(0);
+  const [managerEmail, setManagerEmail] = useState("");
+  const [countyName, setCountyName] = useState("");
 
-    const schema = yup.object().shape({
-        countyName: yup.string().required(),
-        email: yup.string().email().required(),
-    });
-
-    const handleAccountWithTab = (stepIn: number, subValues: any) => {
-        setCurrentStep(stepIn);
-        const account = { name: subValues.countyName, email: subValues.email };
-        if(stepIn === 6)
-            registerAccount(account);
-        else
-            setInstitutionAccount(account);
-    };
-
-    return (
-        <>
-            {type === "county" ? (
-                <Formik
-                    validationSchema={schema}
-                    onSubmit={(subValues: any) => {
-                        subValues ? handleAccountWithTab(step, subValues) : null;
-                    }}
-                    initialValues={{}}
-                >
-                    {({ handleSubmit }) => {
-                        return (
-                            <Form noValidate validated={false}>
-                                <Row className="mb-3">
-                                    <CapFormik
-                                        as={Col}
-                                        label="countyName"
-                                        placeholder="insertCountyName"
-                                        name="countyName"
-                                    />
-                                    <CapFormik
-                                        as={Col}
-                                        label="responsibleEmail"
-                                        placeholder="insertResponsibleEmail"
-                                        name="email"
-                                        type="email"
-                                    />
-                                </Row>
-                                <Row>
-                                    <Col md="auto" className="!pl-0 !pr-3">
-                                        <CapBtn
-                                            label="forwardToAccountable"
-                                            iconType="bi"
-                                            icon="BiMailSend"
-                                            click={() => {
-                                                setStep(6);
-                                                handleSubmit();
-                                            }}
-                                        />
-                                    </Col>
-                                    <Col md="auto" className="!pl-0 !pr-3">
-                                        <CapBtn
-                                            label="forwardToAccountableGoToAutarky"
-                                            iconType="bi"
-                                            icon="BiMailSend"
-                                            click={() => {
-                                                setStep(3);
-                                                handleSubmit();
-                                            }}
-                                        />
-                                    </Col>
-                                    <Col md="auto" className="!p-0">
-                                        <CapBtn
-                                            label="continueFillingOut"
-                                            iconType="md"
-                                            icon="MdNavigateNext"
-                                            click={() => {
-                                                setStep(1);
-                                                handleSubmit();
-                                            }}
-                                        />
-                                    </Col>
-                                </Row>
-                            </Form>
-                        );
-                    }}
-                </Formik>
-            ) : (
-                <Formik
-                    validationSchema={schema}
-                    onSubmit={(e: any) => {
-                        e ? handleAccountWithTab(step, e) : null;
-                    }}
-                    initialValues={{}}
-                >
-                    {({ handleSubmit }) => {
-                        return (
-                            <Form noValidate validated={false}>
-                                <Row className="mb-3">
-                                    <CapFormik
-                                        as={Col}
-                                        label="autarkyName"
-                                        placeholder="insertAutarkyName"
-                                        name="countyName"
-                                    />
-                                    <CapFormik
-                                        as={Col}
-                                        label="responsibleEmail"
-                                        placeholder="insertResponsibleEmail"
-                                        name="email"
-                                        type="email"
-                                    />
-                                </Row>
-                                <Row>
-                                    <Col md="auto" className="!pl-0 !pr-3">
-                                        <CapBtn
-                                            label="forwardToAccountable"
-                                            iconType="bi"
-                                            icon="BiMailSend"
-                                            click={() => {
-                                                setStep(6);
-                                                handleSubmit();
-                                            }}
-                                        />
-                                    </Col>
-                                    <Col md="auto" className="!pl-0 !pr-3">
-                                        <CapBtn
-                                            label="forwardItToAccountable"
-                                            iconType="bi"
-                                            icon="BiMailSend"
-                                            click={() => {
-                                                setStep(3);
-                                                handleSubmit();
-                                            }}
-                                        />
-                                    </Col>
-                                    <Col md="auto" className="!p-0">
-                                        <CapBtn
-                                            label="continueFillingOut"
-                                            iconType="md"
-                                            icon="MdNavigateNext"
-                                            click={() => {
-                                                setStep(4);
-                                                handleSubmit();
-                                            }}
-                                        />
-                                    </Col>
-                                </Row>
-                            </Form>
-                        );
-                    }}
-                </Formik>
-            )}
-        </>
-    );
+  return (
+    <>
+      {/* <CapSubtitle label="account" /> */}
+      <Row>
+        <CapForm
+          as={Col}
+          label={kind === "county" ? "countyName" : "autarkyName"}
+          placeholder={kind === "county" ? "insertCountyName" : "insertAutarkyName"}
+          value={countyName}
+          change={(e: any) => {
+            setCountyName(e.target.value);
+            handleAccount(
+              {
+                name: e.target.value,
+                email: managerEmail,
+              },
+              kind
+            );
+          }}
+        />
+        <CapForm
+          as={Col}
+          label="responsibleEmail"
+          placeholder="insertResponsibleEmail"
+          value={managerEmail}
+          type="email"
+          change={(e: any) => {
+            setManagerEmail(e.target.value);
+            handleAccount(
+              {
+                name: countyName,
+                email: e.target.value,
+              },
+              kind
+            );
+          }}
+        />
+      </Row>
+    </>
+  );
 }
--- components/registration/info.tsx
+++ components/registration/info.tsx
@@ -1,201 +1,71 @@
 import CapForm from "atoms/capForm";
 import CapSubtitle from "atoms/capSubtitle";
-
-import { Formik } from "formik";
-import * as yup from "yup";
-
 import { InfoDTO } from "pages/api/counties";
 import { useEffect, useState } from "react";
-import { Col, Form, Row } from "react-bootstrap";
-import CapFormik from "atoms/capFormik";
-import CapBtn from "atoms/capBtn";
+import { Col, Row } from "react-bootstrap";
 
 type InfoProps = {
-    language?: "pt";
-    info?: InfoDTO;
-    kind: "county" | "autarky";
-    handleInfo: (info: InfoDTO, kind: "county" | "autarky") => void;
-    setState?: any;
-    setActiveTab?: any;
+  language: "pt";
+  info?: InfoDTO;
+  kind: "county" | "autarky";
+  handleInfo: (info: InfoDTO, kind: "county" | "autarky") => void;
 };
 
 export default function Info({
-    language = "pt",
-    info,
-    kind,
-    handleInfo,
-    setState,
-    setActiveTab,
+  language = "pt",
+  info,
+  kind,
+  handleInfo,
 }: InfoProps) {
-    const [countyState, setCountyState] = useState("");
-    const [countyMayor, setCountyMayor] = useState("");
-    const [countyPopulation, setCountyPopulation] = useState("");
-    const [countyAnniversary, setCountyAnniversary] = useState("");
-    const [countyDistanceToCisab, setCountyDistanceToCisab] = useState("");
-    const [countyNote, setCountyNote] = useState("");
+  const [countyState, setCountyState] = useState("");
+  const [countyMayor, setCountyMayor] = useState("");
+  const [countyPopulation, setCountyPopulation] = useState("");
+  const [countyAnniversary, setCountyAnniversary] = useState("");
+  const [countyDistanceToCisab, setCountyDistanceToCisab] = useState("");
+  const [countyNote, setCountyNote] = useState("");
 
-    const save = async () => {
-        let countyData: InfoDTO = {
-            mayor: countyMayor,
-            population: countyPopulation,
-            anniversary: countyAnniversary,
-            distanceToCisab: countyDistanceToCisab,
-            note: countyNote,
-        };
-        handleInfo(countyData, kind);
+  const save = async () => {
+    let countyData: InfoDTO = {
+      mayor: countyMayor,
+      population: countyPopulation,
+      anniversary: countyAnniversary,
+      distanceToCisab: countyDistanceToCisab,
+      note: countyNote,
     };
+    handleInfo(countyData, kind);
+  };
 
-    function getInfo(): InfoDTO {
-        return {
-            anniversary: countyAnniversary,
-            distanceToCisab: countyDistanceToCisab,
-            mayor: countyMayor,
-            note: countyNote,
-            population: countyPopulation,
-        };
+  function getInfo(): InfoDTO {
+    return {
+      anniversary: countyAnniversary,
+      distanceToCisab: countyDistanceToCisab,
+      mayor: countyMayor,
+      note: countyNote,
+      population: countyPopulation,
+    };
+  }
+
+  useEffect(() => {
+    if (info) {
+      setCountyMayor(info?.mayor);
+      setCountyPopulation(info?.population);
+      setCountyAnniversary(info?.anniversary.toString());
+      setCountyDistanceToCisab(info?.distanceToCisab);
+      setCountyNote(info?.note);
     }
+  }, [info]);
 
-    useEffect(() => {
-        if (info) {
-            setCountyMayor(info?.mayor);
-            setCountyPopulation(info?.population);
-            setCountyAnniversary(info?.anniversary.toString());
-            setCountyDistanceToCisab(info?.distanceToCisab);
-            setCountyNote(info?.note);
-        }
-    }, [info]);
-
-    const schema = yup.object().shape({
-        state: yup.string(),
-        mayor: yup.string().required(),
-        population: yup.string(),
-        countyAnniversary: yup.string(),
-        countyDistanceToCisab: yup.string(),
-        note: yup.string(),
-    });
-
-    const [step, setStep] = useState(0);
-
-    const handleAccountWithTab = (s: number, st: any) => {
-        setActiveTab(s);
-        const stateIn = {
-            state: st.state,
-            mayor: st.mayor,
-            population: st.population,
-            countyAnniversary: st.countyAnniversary,
-            countyDistanceToCisab: st.countyDistanceToCisab,
-            note: st.note,
-        };
-        setState(stateIn);
-    };
-
-    return (
-        <>
-            {/* <CapSubtitle label="countyData" /> */}
-            {kind === "county" ? (
-                <Formik
-                    validationSchema={schema}
-                    onSubmit={(e: any) => {
-                        e ? handleAccountWithTab(step, e) : null;
-                    }} //console.log(e.countyName); setActiveTab(step);
-                    initialValues={{}}
-                >
-                    {({
-                        handleSubmit,
-                        handleChange,
-                        values,
-                        errors,
-                        isValid,
-                        isSubmitting,
-                    }) => {
-                        return (
-                            <Form noValidate validated={false}>
-                                <Row className="mb-3">
-                                    <CapFormik
-                                        kind="select"
-                                        as={Col}
-                                        label="state"
-                                        //optionsDefault={1}
-                                        options={["MG"]}
-                                        value={["MG"]} //countyState
-                                        disabled={true}
-                                        name="state"
-                                    />
-                                    <CapFormik
-                                        as={Col}
-                                        label="mayor"
-                                        placeholder="insertMayor"
-                                        name="mayor"
-                                    />
-                                    <CapFormik
-                                        as={Col}
-                                        label="population"
-                                        placeholder="insertPopulation"
-                                        name="population"
-                                        type="number"
-                                    />
-                                </Row>
-                                <Row className="mb-3">
-                                    <CapFormik
-                                        as={Col}
-                                        type="date"
-                                        label="countyAnniversary"
-                                        //placeholder="insertCountyAnniversary"
-                                        name="countyAnniversary"
-                                    />
-                                    <CapFormik
-                                        as={Col}
-                                        label="countyDistanceToCisab"
-                                        placeholder="insertCountyDistanceToCisab"
-                                        name="countyDistanceToCisab"
-                                    />
-                                </Row>
-                                <Row className="mb-3">
-                                    <CapFormik
-                                        as={Col}
-                                        asControl="textarea"
-                                        rows={3}
-                                        label="note"
-                                        placeholder="insertNote"
-                                        name="note"
-                                    />
-                                </Row>
-                                <Row>
-                                    <Col>
-                                        <CapBtn
-                                            kind="next"
-                                            click={() => {
-                                                setStep(2);
-                                                handleSubmit();
-                                            }}
-                                        />
-                                    </Col>
-                                </Row>
-                            </Form>
-                        );
-                    }}
-                </Formik>
-            ) : (
-                <Row>
-                    <Col>
-                        <CapBtn
-                            kind="next"
-                            click={() => {
-                                setActiveTab(5);
-                            }}
-                        />
-                    </Col>
-                </Row>
-            )}
-            {/* <Row className="mb-3">
+  return (
+    <>
+      {/* <CapSubtitle label="countyData" /> */}
+      <Row>
         <CapForm
           kind="select"
           as={Col}
           label="state"
           optionsDefault={1}
           options={["MG"]}
-          value={"MG"} //countyState
-          disabled={true}
+          value={countyState}
           change={(e: any) => {
             setCountyState(e.target.value);
           }}
@@ -226,7 +96,7 @@ export default function Info({
           }}
         />
       </Row>
-      <Row className="mb-3 flex items-center">
+      <Row className="flex items-center">
         <CapForm
           as={Col}
           label="countyAnniversary"
@@ -265,7 +135,7 @@ export default function Info({
           handleInfo({ ...info, note }, kind);
           setCountyNote(e.target.value);
         }}
-      /> */}
-        </>
-    );
+      />
+    </>
+  );
 }
