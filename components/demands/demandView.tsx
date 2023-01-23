81ec9a186ccee8b1ffaf78bb28d9d47f94b98372
Daniela Gomes <daniela.souza.gomes@outlook.com>
2023-01-23 09:31:23 -0300

    Working

--- components/demands/demandView.tsx
+++ components/demands/demandView.tsx
@@ -10,41 +10,49 @@ import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 
 export default function DemandView({ demand }: { demand: DemandDTO }) {
-    console.log("DEMAND");
-    console.log(demand);
-    const size = 7;
+  console.log("DEMAND");
+  console.log(demand);
+  const size = 7;
 
-    const [searchProduct, setSearchProduct] = useState("");
+  const [searchProduct, setSearchProduct] = useState("");
 
-    const [format, setFormat] = useState("grid");
-    const [page, setPage] = useState(0);
-    const [productsPage, setProductsPage] = useState(
-        demand.product_ids.slice(page * size, page * size + size)
-    );
+  const [format, setFormat] = useState("grid");
+  const [page, setPage] = useState(0);
+  const [productsPage, setProductsPage] = useState(
+    demand.product_ids.slice(page * size, page * size + size)
+  );
 
-    let productsPageFinal = productsPage;
-    productsPageFinal.map((p, i) => {
-        p.photo = { photo_url: productsPage[i].photo_url };
-    });
+  // let productsPageFinal = productsPage;
+  // productsPageFinal.map((p, i) => {
+  //     p.photo = { photo_url: productsPage[i].photo_url };
+  // });
+  let productsPageFinal = productsPage;
+  productsPageFinal.map((productId) => {
+    {
+      name: productId;
+      photo_url: "https://mergejil.mn/mergejilmn/no-image.svg";
+    }
+  });
 
-    return (
-        <>
-            <Row>
-            <Col sm={4} className="flex justify-center mb-16 pr-16">
-                    <div className="absolute bg-[silver] w-[200px] h-[200px] rounded-full circle">
-                    </div>
-                    <CapImage
-                        src={"https://mergejil.mn/mergejilmn/no-image.svg"} //county.county.flag
-                        //alt={county.name}
-                        w={200}
-                        h={200}
-                        obj="contain"
-                    />
-                </Col>
-                <Col><CapTitle base="none" literal={demand.name} /></Col>
-            </Row>
-            <Row>
-                    {/*<Col md="auto" className="border-r-2 mr-3 !my-6"> <div className="flex flex-column">
+  return (
+    <>
+      <Row>
+        <Col sm={4} className="flex justify-center mb-16 pr-16">
+          <div className="absolute bg-[silver] w-[200px] h-[200px] rounded-full circle"></div>
+          <CapImage
+            src={"https://mergejil.mn/mergejilmn/no-image.svg"} //county.county.flag
+            //alt={county.name}
+            w={200}
+            h={200}
+            obj="contain"
+          />
+        </Col>
+        <Col>
+          <CapTitle base="none" literal={demand.name} />
+        </Col>
+      </Row>
+      <Row>
+        {/*<Col md="auto" className="border-r-2 mr-3 !my-6"> <div className="flex flex-column">
                         <CapIconButton
                             css="mb-3 mt-6"
                             iconType="fa"
@@ -88,20 +96,18 @@ export default function DemandView({ demand }: { demand: DemandDTO }) {
                         page={page}
                         setPage={setPage}
                     /> </Col>*/}
-                    <CapSwitcher
-                        data={productsPageFinal}
-                        tableHeaders={["image", "products"]}
-                        tableColumns={["photo", "name"]}
-                        tableNumeral={true}
-                        tableImage={1}
-                        buttons={["view"]}
-                        buttonsPaths={[
-                            "/products/",
-                        ]}
-                        searchPath={"name"}
-                        pagesSize = {size}
-                    />
-            </Row>
-        </>
-    );
+        <CapSwitcher
+          data={productsPageFinal}
+          tableHeaders={["products"]}
+          tableColumns={["name"]}
+          tableNumeral={true}
+          //   tableImage={1}
+          buttons={["view"]}
+          buttonsPaths={["/products/"]}
+          //   searchPath={"name"}
+          pagesSize={size}
+        />
+      </Row>
+    </>
+  );
 }
