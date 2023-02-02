e25b9599e95640546cbb52d95aea36616b39ae3d
Daniela Gomes <daniela.souza.gomes@outlook.com>
2023-02-02 07:12:29 -0300

    Consertando botões da demanda

--- pages/api/demands.tsx
+++ pages/api/demands.tsx
@@ -21,19 +21,8 @@ async function handler(req: NextApiRequest, res: NextApiResponse<DemandDTO[]>) {
     return;
   }
 
-  //   if (req.method === "POST") {
-  //     const response = await fetch(process.env.API_URL + "/demands", {
-  //         headers: { Authorization: "Bearer " + user.token, "Content-Type": "application/json" },
-  //         method: "POST",
-  //         body: req.body
-  //     });
-  //     const data = (await response.json()) as DemandDTO[];
-  //     res.status(200).json(data);
-  //     return;
-  // }
-
-  /* if (req.method === "POST") {
-    const response = await fetch(process.env.API_URL + "/counties", {
+  if (req.method === "POST") {
+    const response = await fetch(process.env.API_URL + "/demands", {
       headers: {
         Authorization: "Bearer " + user.token,
         "Content-Type": "application/json",
@@ -41,85 +30,15 @@ async function handler(req: NextApiRequest, res: NextApiResponse<DemandDTO[]>) {
       method: "POST",
       body: req.body,
     });
-    const data = ([{
-      _id: "string1",
-      name: "string1",
-      demandTheme: "string",
-      products: [],
-      startDate: "string",
-      endDate: "string",
-  },
-  {
-      _id: "string2",
-      name: "string2",
-      demandTheme: "string",
-      products: [],
-      startDate: "string",
-      endDate: "string",
-  },
-  {
-      _id: "string3",
-      name: "string3",
-      demandTheme: "string",
-      products: [],
-      startDate: "string",
-      endDate: "string",
-  }]) as DemandDTO[];
-    res.status(200).json(data);
+    const data = (await response.json()) as DemandDTO[];
+    res.status(response.status).json(data);
     return;
-  } */
+  }
 
   const response = await fetch(process.env.API_URL + "/demands", {
     headers: { Authorization: "Bearer " + user.token },
   });
-  /*  const data = ([{
-    id: "string1",
-    name: "Restauração Elétrica",
-    demandTheme: "Restauração Elétrica",
-    products: [{
-      "_id": "63a0ba62610ae58ba6d8f9aa",
-      "name": "produto 1",
-      "measurements": [
-          {
-              "name": "comprimento",
-              "value": "3",
-              "unit": "cm"
-          },
-          {
-              "name": "largura",
-              "value": "5",
-              "unit": "cm"
-          }
-      ],
-      "norms": [
-          "string"
-      ],
-      "code": "123fd",
-      "accessory_ids": [],
-      "categories": [
-          "Hidraulico"
-      ],
-      "photo_url": "https://i.ibb.co/6tmJqXm/SEM-IMAGEM.png"
-  }],
-    startDate: "19/07/2023",
-    endDate: "12/08/2023",
-},
-{
-    id: "string2",
-    name: "Peças de Reposição",
-    demandTheme: "Peças de Reposição",
-    products: [],
-    startDate: "03/06/2023",
-    endDate: "23/11/202320",
-},
-{
-    id: "string3",
-    name: "Equipamentos Hidráulicos",
-    demandTheme: "Equipamentos Hidráulicos",
-    products: [],
-    startDate: "01/01/2023",
-    endDate: "01/03/2023",
-}]) as DemandDTO[];  */
+
   const data = (await response.json()) as DemandDTO[];
   console.log("DATA");
   console.log(data);
