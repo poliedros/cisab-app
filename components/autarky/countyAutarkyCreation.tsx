ffcdda342b818214a5a7070b60100fe6ba1c8c07
MrAndersonMR <sr.andersonmendesribeiro@gmail.com>
2023-02-04 12:15:02 -0300

    start changes to autarky

---
+++ components/autarky/countyAutarkyCreation.tsx
@@ -0,0 +1,8 @@
+import { CountyDTO } from "pages/api/counties";
+
+export default function CountyAutarkyCreation({ county }: { county: CountyDTO }) {
+    return (
+        <>
+        </>
+    );
+}
---
+++ components/autarky/countyAutarkyProfile.tsx
@@ -0,0 +1,8 @@
+import { CountyDTO } from "pages/api/counties";
+
+export default function CountyAutarkyProfile({ county }: { county: CountyDTO }) {
+    return (
+        <>
+        </>
+    );
+}
