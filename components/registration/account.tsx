cbe2354ea2fcd8182815aed04dc9e762457656c9
Daniela Gomes <daniela.souza.gomes@outlook.com>
2023-02-04 13:35:48 -0300

    Inserindo campo para pegar  municipio atraves de lista

--- components/registration/account.tsx
+++ components/registration/account.tsx
@@ -1,8 +1,11 @@
 import CapForm from "atoms/capForm";
+import CapInputAdvanced from "atoms/capInputAdvanced";
 import CapSubtitle from "atoms/capSubtitle";
+import { CategoryDTO } from "pages/api/categories";
 import { CountyManagerDTO } from "pages/api/counties/[id]/manager";
 import { useState } from "react";
 import { Row, Col } from "react-bootstrap";
+import { MutatorCallback, MutatorOptions } from "swr";
 
 type AccountProps = {
   kind: "county" | "autarky";
@@ -12,12 +15,24 @@ type AccountProps = {
   ) => void;
 };
 
-export default function Account({
-  kind,
-  handleAccount,
-}: AccountProps) {
+export default function Account({ kind, handleAccount }: AccountProps) {
   const [managerEmail, setManagerEmail] = useState("");
   const [countyName, setCountyName] = useState("");
+
+  const [cities, setCities] = useState<any[]>([]);
+
+  const getCities = async () => {
+    const data = await fetch("/api/ibge/mgCounties", {
+      method: "GET",
+    });
+
+    if (data.status != 200) console.log("Problema com a API do IBGE");
+
+    const result = await data.json();
+    setCities(result);
+  };
+
+  getCities();
 
   return (
     <>
@@ -26,7 +41,9 @@ export default function Account({
         <CapForm
           as={Col}
           label={kind === "county" ? "countyName" : "autarkyName"}
-          placeholder={kind === "county" ? "insertCountyName" : "insertAutarkyName"}
+          placeholder={
+            kind === "county" ? "insertCountyName" : "insertAutarkyName"
+          }
           value={countyName}
           change={(e: any) => {
             setCountyName(e.target.value);
@@ -37,6 +54,25 @@ export default function Account({
               },
               kind
             );
+          }}
+        />
+        <CapInputAdvanced
+          kind="base"
+          label="searchCountyByName"
+          placeholder="insertCountyName"
+          base="filter"
+          values={cities?.map((c) => c.name)}
+          array={cities}
+          setArray={setCities}
+          mutate={function (
+            data?:
+              | CategoryDTO[]
+              | Promise<CategoryDTO[]>
+              | MutatorCallback<CategoryDTO[]>
+              | undefined,
+            opts?: boolean | MutatorOptions<CategoryDTO[]> | undefined
+          ): Promise<CategoryDTO[] | undefined> {
+            throw new Error("Function not implemented.");
           }}
         />
         <CapForm
