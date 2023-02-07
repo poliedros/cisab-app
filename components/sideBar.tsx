290f5af16ccaae3c6909b2e558d16c7d1bd6b4a2
Daniela <daniela.souza.gomes@outlook.com>
2023-02-07 14:39:39 -0300

    Linkando o menu a rota pagina de demandas abertas

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -141,12 +141,12 @@ export default function SideBar() {
           }
         >
           <CapIconButton
-            iconType="md"
-            icon={iconBrightness}
+            iconType="fa"
+            icon="FaThList"
             size="24px"
-            //click={handleBrightness}
+            route="/demands/viewer"
             hoverColor="#7dc523"
-            tooltip="themes"
+            tooltip="listDemands"
           />
         </div>
       </div>
