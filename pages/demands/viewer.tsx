850b422fb041416af563357fd3e8693eee68295a
Daniela <daniela.souza.gomes@outlook.com>
2023-02-07 14:44:24 -0300

    Filtrando apenas demandas abertas

--- pages/demands/viewer.tsx
+++ pages/demands/viewer.tsx
@@ -1,22 +1,27 @@
 import DemandListView from "components/noAccount/demandListView";
+import { now } from "d3";
 import useUser from "lib/useUser";
 import { DemandDTO } from "pages/api/demands";
 import useSWR from "swr";
 
 export default function Viewer() {
-    const { user } = useUser({ redirectTo: "/login" });
+  const { user } = useUser({ redirectTo: "/login" });
 
-    const { data: demands, error } = useSWR<DemandDTO[]>(
-        user ? "/api/demands" : null
-    );
+  const { data: demands, error } = useSWR<DemandDTO[]>(
+    user ? "/api/demands" : null
+  );
 
-    if (!user || user.isLoggedIn == false) {
-        return <div>404</div>;
-    }
+  if (!user || user.isLoggedIn == false) {
+    return <div>404</div>;
+  }
 
-    return (
-        <>
-            <DemandListView demands={demands ? demands : []} />
-        </>
-    );
+  const openDemands = demands?.filter(
+    (d) => Date.parse(d.start_date) > Date.now()
+  );
+
+  return (
+    <>
+      <DemandListView demands={openDemands ? openDemands : []} />
+    </>
+  );
 }
