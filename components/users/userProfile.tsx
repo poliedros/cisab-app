4837743527583ae80eb6313c8ce92ba7bdd1cfad
Daniela Gomes <daniela.souza.gomes@outlook.com>
2024-11-10 17:40:07 -0300

    update user profile

--- components/users/userProfile.tsx
+++ components/users/userProfile.tsx
@@ -61,6 +61,7 @@ export default function UserProfile({}: //countyUser,
     user_id: string,
     edit_county_data: boolean
   ) => {
+    console.log(user?.roles);
     if (edit_county_data) Router.push(`${prefix}${county_id}/edit`);
     else Router.push(`${prefix}${user_id}/edit`);
   };
@@ -74,9 +75,10 @@ export default function UserProfile({}: //countyUser,
               <ul className="menu">
                 <li>
                   <CapIconButton
-                    iconType="ri"
-                    icon="RiEyeFill"
+                    iconType="fa"
+                    icon="FaUserEdit"
                     size="24px"
+                    tooltip="editUserTooltip"
                     click={() =>
                       editUser(
                         "/users/",
@@ -94,6 +96,7 @@ export default function UserProfile({}: //countyUser,
                       iconType="ri"
                       icon="RiEditBoxFill"
                       size="24px"
+                      tooltip="editInstitutionTooltip"
                       click={() =>
                         editUser(
                           "/counties/",
@@ -108,14 +111,6 @@ export default function UserProfile({}: //countyUser,
                 ) : null}
               </ul>
             </div>
-            {/* <CapImage
-              src={"https://mergejil.mn/mergejilmn/no-image.svg"} //county.county.flag
-              //alt={county.name}
-              w={200}
-              h={200}
-              obj="contain"
-            /> */}
-
             <div className="z-10 rotate-center">
               {user?.roles.includes("cisab") ? (
                 <CapImage
@@ -165,13 +160,6 @@ export default function UserProfile({}: //countyUser,
                     : translations("noValue", "pt")
                   : translations("noValue", "pt")}
               </h6>
-              {/* <CapParagraph
-                literal={
-                  countyUser.email
-                    ? countyUser.email
-                    : translations("noValue", "pt")
-                }
-              /> */}
               <CapLink
                 literal={user ? user.email : translations("noValue", "pt")}
                 icon="MdAlternateEmail"
@@ -179,113 +167,22 @@ export default function UserProfile({}: //countyUser,
                 iconColor="text-[#144974]"
                 href={`mailto:${user ? user.email : null}?subject=`}
               />
-              {/* <h5 className="text-[#dd823b] my-2">
-                {countyUser.name
-                  ? countyUser.name
-                  : translations("noValue", "pt")}
-              </h5> */}
             </Row>
             <Row className="border-t-2 pt-6 m-4 items-center w-max width-f-available">
               <Col className="text-left my-2">
                 {!user?.roles.includes("cisab") ? (
                   <CapTextShowData
-                    label={"county"}
+                    label={"institutionName"}
                     info={county ? county.name : translations("noValue", "pt")}
                   />
                 ) : (
                   <></>
                 )}
-                {/* <CapTextShowData
-                  label={"zipCode"}
-                  info={
-                    countyUser.email
-                      ? countyUser.email
-                      : translations("noValue", "pt")
-                  }
-                />
-                <CapTextShowData
-                  label={"countyDistanceToCisab"}
-                  info={
-                    countyUser.name
-                      ? countyUser.name
-                      : translations("noValue", "pt")
-                  }
-                />
-                <CapTextShowData
-                  label={"mayor"}
-                  info={
-                    countyUser.properties
-                      ? countyUser.properties.profession
-                      : translations("noValue", "pt")
-                  }
-                />
-                <CapTextShowData
-                  label={"population"}
-                  info={
-                    countyUser.properties
-                      ? countyUser.properties.profession
-                      : translations("noValue", "pt")
-                  }
-                />
-                <CapTextShowData
-                  label={"countyAnniversary"}
-                  info={
-                    countyUser.surname
-                      ? countyUser.surname
-                      : translations("noValue", "pt")
-                  }
-                /> */}
               </Col>
-              {/* <Col className="text-left bg-[#f9f9f9] !px-8">
-                <CapInfoBoard title={"lastActions"} />
-              </Col> */}
             </Row>
-            {/* <Row className="text-center width-f-available">
-              <CapParagraph
-                literal={
-                  countyUser.name
-                    ? countyUser.name
-                    : translations("noValue", "pt")
-                }
-              />
-              <CapParagraph
-                literal={
-                  countyUser.email
-                    ? countyUser.email
-                    : translations("noValue", "pt")
-                }
-              />
-            </Row> */}
           </Col>
         </Row>
       </div>
     </>
-    // <>
-    //   <Row className="border-t-2 pt-6 m-4 items-center">
-    //     <Col className="text-left my-2">
-    //       <CapForm label={"name"} value={countyUser.name} />
-    //       <CapForm label={"surname"} value={countyUser.surname} />
-    //       <CapForm label={"email"} value={countyUser.email} />
-    //       <CapForm label={"password"} value={countyUser.password} />
-    //       <CapForm label={"job"} value={countyUser.properties.profession} />
-    //     </Col>
-    //   </Row>
-    //   <CapBtn
-    //     label="submit"
-    //     click={() => {
-    //       registerUser(countyUser);
-    //     }}
-    //   />
-    //   {errorMessage ? (
-    //     <CapMessageBottom label={"passwordError"} css="text-red-600" />
-    //   ) : (
-    //     <></>
-    //   )}
-    //   {successMessage ? (
-    //     <CapMessageBottom label={"passwordSuccess"} css="text-green-600" />
-    //   ) : (
-    //     <></>
-    //   )}
-    // </>
   );
 }
